94e493363ee0aefc2784e4b831638f358e37d489
Yoan Thirion <yoan.thirion@gmail.com>
2022-06-03 10:51:16 +0200

    Add C# version

---
+++ c#/UnitTestIntro/UnitTestsIntro.Tests/CalculatorShould.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace UnitTestsIntro.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(false);
+    }
+}
---
+++ c#/UnitTestIntro/UnitTestsIntro/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestsIntro;
+
+public static class Calculator
+{
+    public const string Add = "add";
+    public const string Multiply = "multiply";
+    public const string Divide = "divide";
+    public const string Subtract = "subtract";
+
+    private static readonly Dictionary<string, Func<int, int, int>> _supportedOperators =
+        new()
+        {
+            {Add, (a, b) => a + b},
+            {Multiply, (a, b) => a * b},
+            {Divide, (a, b) => a / b},
+            {Subtract, (a, b) => a - b}
+        };
+    public static int Calculate(int a, int b, string @operator) =>
+        _supportedOperators.ContainsKey(@operator)
+            ? _supportedOperators[@operator](a, b)
+            : throw new ArgumentException("Not supported operator");
+}
