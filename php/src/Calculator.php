2a0a84de72e80bd1c5600237bfa313c8f10ef62e
Jean-Philippe Bousselin <ssj@foyer.lu>
2022-06-13 18:42:57 +0200

    added calculator code

--- php/src/Calculator.php
+++ php/src/Calculator.php
@@ -2,7 +2,19 @@
 
 class Calculator() {
 
-  
+  private $supportedOperators = [
+    "add" => function(a, b) { return a + b},
+    "multiply" => function(a, b) { return a * b},
+    "divide" => function(a, b) { return a / b},
+    "substract" => function(a, b) { return a - b}
+  ]
+
+  public static function calculate($a, $b, $operator) {
+    if(!array_key_exists($operator, $this->supportedOperators)) {
+      throw new Exception("Unsupported operator {$operator}")
+    }
+    return $this->supportedOperators[$operator](a, b);
+  }
 
 }
 
