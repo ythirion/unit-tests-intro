184faf417d76ed5af00cd4b1d8715e3b2faca008
Jean-Philippe Bousselin <ssj@foyer.lu>
2022-06-15 18:28:07 +0200

    PHP: added last tests

--- solutions/php/tests/TimeUtilityTests.php
+++ solutions/php/tests/TimeUtilityTests.php
@@ -1,20 +1,53 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . "../../src/TimeUtility.php";
 
-class TimeUtilityTests extends TestCase
-{
+class TimeUtilityTests extends TestCase {
 
-  public function test6AMShouldBeMorning() {
+    protected $clockStub;
 
-    $stub = $this->createStub(Clock::class);
+    protected function setUp() : void
+    {
+        $this->clockStub = $this->createStub(Clock::class);
+    }
 
-    $stub->method('time')
-         ->willReturn(new DateTime('2011-01-01T06:00:00.012345Z'));
+    public function test6AMShouldBeMorning()
+    {
+        $this->clockStub
+             ->method('time')
+             ->willReturn(new DateTime('2011-01-01T06:00:00.012345Z'));
 
-    $this->assertSame("Morning", TimeUtility::timeOfDay($stub));
-  }
+        $this->assertSame("Morning", TimeUtility::timeOfDay($this->clockStub));
+    }
+
+    public function test1PMShouldBeAfternoon()
+    {
+        $this->clockStub
+            ->method('time')
+            ->willReturn(new DateTime('2011-01-01T13:00:00.012345Z'));
+
+        $this->assertSame("Afternoon", TimeUtility::timeOfDay($this->clockStub));
+    }
+
+    public function test2AMShouldBeNight()
+    {
+        $this->clockStub
+            ->method('time')
+            ->willReturn(new DateTime('2011-01-01T02:00:00.012345Z'));
+
+        $this->assertSame("Night", TimeUtility::timeOfDay($this->clockStub));
+    }
+
+    public function test11PMShouldBeEvening()
+    {
+        $this->clockStub
+            ->method('time')
+            ->willReturn(new DateTime('2011-01-01T23:00:00.012345Z'));
+
+        $this->assertSame("Evening", TimeUtility::timeOfDay($this->clockStub));
+    }
 
 }
 
