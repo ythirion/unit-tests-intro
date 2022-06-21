11958b33965ba2974b4022234c986386690f6ba9
Jean-Philippe Bousselin <ssj@foyer.lu>
2022-06-21 19:06:12 +0200

    added Operators record

--- solutions/fsharp/UnitTestsIntro/Tests.fs
+++ solutions/fsharp/UnitTestsIntro/Tests.fs
@@ -2,10 +2,21 @@ module Tests
 open System
 open Xunit
 
+
+let add a b = a + b
+
+type Operators =
+    | Add
+    | Multiply
+    | Divide
+    | Subtract
+
 let calculate a b op =
     match op with
-    | "add" -> a + b
-    | "multiply" -> a * b
+    | Operators.Add -> a + b
+    | Operators.Multiply -> a * b
+    | Operators.Divide -> a / b
+    | Operators.Subtract -> a - b
     | _ -> 0
 
 [<Fact>]
@@ -14,10 +25,10 @@ let ``My test`` () =
 
 [<Fact>]    
 let ``just trying shit out`` () =
-    Assert.True(calculate 1 1 "add" = 2)
-    Assert.False(calculate 1 1 "add" = 3)
-    Assert.True(calculate 1 2 "multiply" = 2)
-    Assert.False(calculate 2 2 "multiply" = 8)
+    Assert.True(calculate 1 1 Operators.Add = 2)
+    Assert.False(calculate 1 1 Operators.Add = 3)
+    Assert.True(calculate 1 2 Operators.Multiply = 2)
+    Assert.False(calculate 2 2 Operators.Multiply = 8)
     
     
     
