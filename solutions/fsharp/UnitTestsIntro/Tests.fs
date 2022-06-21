43af341308a43e24784138ab4d963c59272f5feb
Jean-Philippe Bousselin <ssj@foyer.lu>
2022-06-21 19:26:23 +0200

    profanity fix

--- solutions/fsharp/UnitTestsIntro/Tests.fs
+++ solutions/fsharp/UnitTestsIntro/Tests.fs
@@ -23,7 +23,7 @@ let ``My test`` () =
     Assert.True(true)
 
 [<Fact>]    
-let ``just trying shit out`` () =
+let ``just trying things :) out`` () =
     Assert.True(calculate 1 1 Operators.Add = 2)
     Assert.False(calculate 1 1 Operators.Add = 3)
     Assert.True(calculate 1 2 Operators.Multiply = 2)
